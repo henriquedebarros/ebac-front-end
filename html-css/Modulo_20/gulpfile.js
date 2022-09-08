60f5b79d812afa09dd2781aa8ca0f14411a3ab2b
Henrique de Barros <henrique.dias.soares@gmail.com>
2022-09-08 18:36:45 +0100

    feat : colocando index em src e dist

--- html-css/Modulo_20/gulpfile.js
+++ html-css/Modulo_20/gulpfile.js
@@ -1,12 +1,14 @@
+const {series}  = require("gulp");
 const gulp  = require("gulp");
 const concat = require("gulp-concat");
 const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
 const uglify = require("gulp-uglify");
 const image = require("gulp-image");
+const htmlmin = require("gulp-htmlmin");
 
-function tarefasCSS() {
-    return gulp.src([
+function tarefasCSS(callback) {
+    gulp.src([
             "./node_modules/bootstrap/dist/css/bootstrap.css",
             "./node_modules/@fortawesome/fontawesome-free/css/fontawesome.css",
             "./node_modules/@fortawesome/fontawesome-free/css/brands.css",
@@ -17,10 +19,12 @@ function tarefasCSS() {
         .pipe(cssmin())
         .pipe(rename({suffix: ".min"}))
         .pipe(gulp.dest("./dist/css"))
+
+    return callback()
 }
 
-function tarefasJS() {
-    return gulp.src([
+function tarefasJS(callback) {
+    gulp.src([
             './node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
             "./node_modules/@fortawesome/fontawesome-free/js/fontawesome.js",
@@ -32,6 +36,8 @@ function tarefasJS() {
         .pipe(uglify())
         .pipe(rename({suffix: ".min"}))
         .pipe(gulp.dest("./dist/js"))
+
+    return callback()
 }
 
 function tarefasImagem() {
@@ -50,6 +56,16 @@ function tarefasImagem() {
         .pipe(gulp.dest("./dist/images"))
 }
 
+function tarefasHTML(callback) {
+    gulp.src("./src/**/*.html")
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("./dist"))
+
+    return callback()
+}
+
 exports.estilos = tarefasCSS;
 exports.scripts = tarefasJS;
 exports.imagens = tarefasImagem;
+
+exports.default = series(tarefasHTML, tarefasJS, tarefasCSS);
