51d801b28a017bb4201c6208403951c731112b4a
Henrique de Barros <henrique.dias.soares@gmail.com>
2022-09-16 17:49:36 +0100

    feat: clone do repo anterior

---
+++ html-css/Modulo_26/README.md
@@ -0,0 +1,41 @@
+# Módulo 25
+
+Projeto utilizando Framework Webpack com servidor ao vivo
+
+## Instalação
+
+Como o node instalado, acessar o diretório do projeto e rodar.
+
+```
+npm install
+```
+
+## Rodando servidor ao vivo
+
+O servidor que auto atualiza a cada alteração no source é acessado pelo comando
+
+```
+npm run dev
+```
+
+Ele fica disponível no endereço
+
+```
+http://localhost:3000/
+```
+
+## Geração do diretório de Distribuição
+
+Depois de baixar os módulos necessários, na pasta raiz usar o comando a seguir para contruir o diretório "./dist" para ver o projeto final.
+
+```
+npm run build
+```
+
+## Acesso a página
+
+A página principal fica localizada em
+
+```
+./dist/index.html
+```
---
+++ html-css/Modulo_26/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpack = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        watchFiles: ["./src/"],
+        port: 3000,
+        open: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|c|sc)ss$/i, 
+                use: [
+                    MiniCssExtract.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(jpeg|jpg|png|svg|gif)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: './assets/[name].[ext]'
+                    }
+                }
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpack({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: 'style.css'
+        })
+    ]
+}
