8dc37b9396569aa65e44302910a45ad8b07d16fa
Henrique de Barros <henrique.dias.soares@gmail.com>
2022-09-16 13:03:06 +0100

    fix: update do webpack

--- html-css/Modulo_24/src/index.js
+++ html-css/Modulo_24/src/index.js
@@ -1,8 +1,25 @@
+import "./style.scss";
+import img from './carro_audi.jpg';
+
 function titleComponent(){
-    const elemH1 = document.createElement('h1')
-    elemH1.innerHTML = 'Hello world 6'
-    elemH1.classList.add('title')
-    return elemH1
+    const elemH1 = document.createElement('h1');
+    elemH1.innerHTML = 'Exercício Webpack';
+    elemH1.classList.add('title');
+    return elemH1;
 }
 
-document.getElementById('root').appendChild(titleComponent())
+function bodyComponent(){
+    const elemBody = document.createElement('p');
+    elemBody.innerHTML = 'Nova forma de gerar conteúdo de forma dinâmica usando html, csss e js auto compilados.';
+    return elemBody;
+}
+
+function imageComponent(){
+    const elemImg = new Image();
+    elemImg.src = img;
+    return elemImg;
+}
+
+document.getElementById('root').appendChild(titleComponent());
+document.getElementById('root').appendChild(bodyComponent());
+document.getElementById('root').appendChild(imageComponent());
--- html-css/Modulo_24/webpack.config.js
+++ html-css/Modulo_24/webpack.config.js
@@ -17,6 +17,32 @@ module.exports = {
                     "css-loader",
                     "sass-loader"
                 ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.(jpeg|jpg|png|svg|gif)$/i,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]"
+                    }
+                }
             }
         ]
 
@@ -25,6 +51,9 @@ module.exports = {
         new htmlWebpack({
             filename: "index.html",
             template: "./src/index.html"
+        }),
+        new miniCssExtract({
+            filename: "style.css"
         })
     ]
 }
