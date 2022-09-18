88890f5cf7ea7df8affb672914573dfd1862c191
Henrique de Barros <henrique.dias.soares@gmail.com>
2022-09-18 13:32:40 +0100

    feat: atualizando imagens no assets

--- html-css/Modulo_26/src/js/index.js
+++ html-css/Modulo_26/src/js/index.js
@@ -2,3 +2,31 @@ import '../scss/styles.scss';
 
 const elemRoot = document.getElementById('root');
 elemRoot.classList.add('container');
+
+document.querySelector(".header_hero").setAttribute("src", "./assets/header_hero.jpg");
+
+for (let i = 1; i < 7; i++) {
+    let seletor = "vitrine__image-" + i;
+    console.log(seletor);
+    document.querySelector("." + seletor).setAttribute("src", "./assets/" + seletor + ".jpg");
+}
+
+const btnEnviar = document.querySelector(".contato__button");
+const inpNome = document.getElementById("nome");
+const inpEmail = document.getElementById("email");
+
+btnEnviar.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(inpNome.value == "" || inpEmail.value == "") {
+        if(inpNome.value == "")
+            alert("Preencha o campo Nome");
+        if(inpEmail.value == "")
+            alert("Preencha o campo E-mail");
+    }
+    else {
+        inpNome.value = "";
+        inpEmail.value = "";
+        alert("Cadastro feito");
+    }
+})
