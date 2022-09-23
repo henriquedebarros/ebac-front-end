f8340247571f81d944972c2d317d5fe2bddacaca
Henrique de Barros <henrique.dias.soares@gmail.com>
2022-09-23 23:05:59 +0100

    feat: update e aplicacao de JS
    README atualizado
    Projeto rodando com interacao

--- html-css/Modulo_29/react-tutorial/src/Table.js
+++ html-css/Modulo_29/react-tutorial/src/Table.js
@@ -1,5 +1,3 @@
-import React, { Component } from 'react'
-
 const TableHeader = () => {
     return (
         <thead>
@@ -17,6 +15,9 @@ const TableBody = (props) => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
     })
@@ -24,17 +25,15 @@ const TableBody = (props) => {
     return <tbody>{rows}</tbody>
 }
 
-class Table extends Component {
-    render() {
-        const { characterData } = this.props
-        return (
-            <table>
-            <TableHeader />
-            <TableBody characterData={characterData} />
-            </table>
-        )
-    }
-}
+const Table = (props) => {
+    const { characterData, removeCharacter } = props
 
+    return (
+        <table>
+        <TableHeader />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    )
+}
 
 export default Table
