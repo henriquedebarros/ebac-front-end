477580c1c007fe364d845f0edcea510fbf41f7cc
Henrique de Barros <henrique.dias.soares@gmail.com>
2022-10-02 15:48:02 +0100

    feat: aplicacao de responsividade

--- html-css/Modulo_35/src/js/index.js
+++ html-css/Modulo_35/src/js/index.js
@@ -3,6 +3,11 @@ import '../scss/styles.scss';
 const botao = document.querySelectorAll(".controle__botao");
 const indicador = document.querySelectorAll(".controle__indicador");
 const banner = document.querySelector(".slides");
+const containerBanner = document.querySelector(".banner");
+let tamanhoBanner = containerBanner.offsetWidth;
+
+console.log(tamanhoBanner);
+
 let pagina = 1;
 desabilita(pagina);
 indica(pagina);
@@ -22,11 +27,13 @@ function indica(pag) {
 }
 
 botao[0].addEventListener("click", () => {
+    tamanhoBanner = containerBanner.offsetWidth;
+
     if(!botao[0].classList.contains("disabled")) {
         pagina--;
     }
     // Fazer o translate
-    let mover = (pagina - 1) * -984;
+    let mover = (pagina - 1) * -tamanhoBanner;
     console.log(mover);
     banner.style.transform = `translate(${mover}px, 0)`;
     desabilita(pagina);
@@ -34,10 +41,12 @@ botao[0].addEventListener("click", () => {
 })
 
 botao[1].addEventListener("click", () => {
+    tamanhoBanner = containerBanner.offsetWidth;
+
     if(!botao[1].classList.contains("disabled")) {
         pagina++;
     }
-    let mover2 = (pagina - 1) * -984;
+    let mover2 = (pagina - 1) * -tamanhoBanner;
     console.log(mover2);
     banner.style.transform = `translate(${mover2}px, 0)`;
     desabilita(pagina);
