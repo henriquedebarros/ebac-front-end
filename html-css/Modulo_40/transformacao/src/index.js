ca310bb061e02f056281f549001f96a6d092713b
Henrique de Barros <henrique.dias.soares@gmail.com>
2022-10-23 20:34:21 +0100

    Initial commit

---
+++ html-css/Modulo_40/transformacao/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>EcoIlhas</h1>
+  // https://www.google.com/maps/d/u/0/edit?mid=1sYb1zcsa-FV9WV6RKypbLasJWZRhttk&usp=sharing
+);
